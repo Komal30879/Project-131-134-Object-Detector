fa2b9b65d4d9f9c7aa4a61b5d79d330d0d7645d0
Komal30879 <82501759+Komal30879@users.noreply.github.com>
2022-04-13 19:41:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+img1 = "";
+img2 = "";
+img3 = "";
+img4 = "";
+img5 = "";
+
+function preload(){
+img1 = loadImage("bed.jpg");
+img2 = loadImage("fruits.jpg");
+img3 = loadImage("bottle.jpg");
+img4 = loadImage("table.jpg");
+img5 = loadImage("tv.jpg");
+}
